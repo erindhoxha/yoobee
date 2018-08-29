78942cea232689d0c577d5cc52687c9ae475c9a2
Erind Hoxha <erind_cbh@hotmail.com>
2018-08-30 10:48:29 +1200

    Removed background, added new feature with jQuery

--- vendor/js/js.js
+++ vendor/js/js.js
@@ -1,7 +1,7 @@
 
 $("#button-scroll").click(function() {
     $('html,body').animate({
-        scrollTop: $("#portfolio-work").offset().top},
+        scrollTop: $("#skill-images").offset().top},
         'slow');
 });
 
@@ -10,6 +10,10 @@ $(function () {
     $(document).scroll(function () {
       var $nav = $(".navbar-fixed-top");
       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      var $img = $("#imagelogo");
+      $img.toggleClass('invert', $(this).scrollTop() > $img.height());
+      var $navLink = $(".nav-link");
+      $navLink.toggleClass('invert', $(this).scrollTop() > $navLink.height());
     });
   });
 
@@ -18,5 +22,28 @@ $(function () {
       var $nav = $(".light-navbar-fixed-top");
       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     });
+  });
+$(".all-button").on('click',function() {
+  $(".web").show(500);
+  $(".mobile").show(500);
+  $(".design").show(500);
+
+})
+  $(".mobile-button").on('click' ,function() {
+    $(".web").css("display","none");
+    $(".design").css("display","none");
+    $(".mobile").css("display","block");
+  });
+
+  $(".web-button").on('click' ,function() {
+    $(".mobile").css("display","none");    
+    $(".web").css("display","block");
+    $(".design").css("display","none");
+  });
+
+  $(".design-button").on('click' ,function() {
+    $(".mobile").css("display","none");
+    $(".design").css("display","block");
+    $(".web").css("display","none");
   });
 
